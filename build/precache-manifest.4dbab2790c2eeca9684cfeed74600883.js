40c05412a8d1ee3e6b119c67373441159effed8d
Martin Banks <anotherbanksy@gmail.com>
2019-02-12 14:08:56 +1100

    adds production build

---
+++ build/precache-manifest.4dbab2790c2eeca9684cfeed74600883.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d0a940f7fd9c31e77871",
+    "url": "/static/js/main.d0a940f7.chunk.js"
+  },
+  {
+    "revision": "77958300264c6869a197",
+    "url": "/static/js/1.77958300.chunk.js"
+  },
+  {
+    "revision": "d0a940f7fd9c31e77871",
+    "url": "/static/css/main.bf73c930.chunk.css"
+  },
+  {
+    "revision": "aeb5f9f89042a29a3e8e8fbb352c84eb",
+    "url": "/index.html"
+  }
+];
